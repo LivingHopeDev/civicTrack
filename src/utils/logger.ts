e59240c83b7a6cbaee1a2eebbd89661fdbe47d42
adetayo adewobi <adewobiadetayo1@gmail.com>
2024-11-09 02:08:14 +0100

    fix: logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,19 @@
 import pino from "pino";
 import dayjs from "dayjs";
 
-const log = pino({
-    transport: {
-        target: "pino-pretty",
-    },
-    base: {
-        pid: false,
-    },
-    timestamp: () => `,"time":"${dayjs().format()}"`,
-});
+const isDev = process.env.NODE_ENV === "development";
+const log = pino(
+  isDev
+    ? {
+        transport: {
+          target: "pino-pretty",
+        },
+        base: {
+          pid: false,
+        },
+        timestamp: () => `,"time":"${dayjs().format()}"`,
+      }
+    : {}
+);
 
 export default log;
